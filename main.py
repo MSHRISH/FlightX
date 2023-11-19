5793d7fdfdcd7a485525851b1ff644fb0d488cb2
Shrishrec236 <200701236@rajalakshmi.edu.in>
2023-11-19 23:46:27 +0530

    Search Flight for Users

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from API import app
 
 if __name__=="__main__":
-    # app.run(debug=False,host='0.0.0.0')
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
+    # app.run(debug=True)
 
